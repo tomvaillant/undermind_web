fc1d92025171a97cacfa748ea29af108e35ebaa4
tom vaillant <chroniclr@hushmail.com>
2018-12-23 12:40:27 +0100

    environment variables done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ config.webpacker.check_yarn_integrity = false
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "UNDERMIND_WEB_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.read_encrypted_secrets = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,4 +1,2 @@
-# ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
-ForestLiana.env_secret = Figaro.env.forest_env_secret
-# ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
-ForestLiana.auth_secret = Figaro.env.forest_auth_secret
+ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
+ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
