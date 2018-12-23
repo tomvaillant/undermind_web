ab6717a97c730f073c1d0556dcd246c8627db233
tom vaillant <chroniclr@hushmail.com>
2018-12-23 12:22:46 +0100

    still testing...

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,6 +1,4 @@
-Figaro.require_keys("forest_auth_secret", "forest_env_secret")
 # ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
 ForestLiana.env_secret = Figaro.env.forest_env_secret
 # ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
 ForestLiana.auth_secret = Figaro.env.forest_auth_secret
-# ForestLiana.auth_secret = Rails.application.application.forest_auth_secret
